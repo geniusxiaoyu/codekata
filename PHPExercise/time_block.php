a81de22e3083de29ae8db04f3e2f2079439def6a
yugang <yugang@xin.com>
2019-08-20 19:02:25 +0800

    完善时间块统计脚本

--- PHPExercise/time_block.php
+++ PHPExercise/time_block.php
@@ -61,11 +61,18 @@ function recordTimeBlock($inputPath, $beginDate, $endDate)
 	$conn->close();
 }
 
-
-
-function statTimeBlock($outputPath, $beginDate, $endDate)
+function outputToCsv($handle, $content)
 {
-	$conn = connDb();
+	$data = [];
+	foreach ($content as $value) {
+		$data[] = iconv('UTF-8', 'GBK//TRANSLIT', $value);
+	}
+	fputcsv($handle, $data);
+	//fputs($handle, iconv('UTF-8', 'GBK//TRANSLIT', implode(",", $content) . "\n"));
+}
+
+function exportToExcel($conn, $outputPath, $beginDate, $endDate)
+{
 	$count = 0;
 	// 输出当月内容
 	$exportSql = "select begin_time, duration_time, event_type, content, remark from time_block where  begin_time >= '{$beginDate}' and begin_time < '{$endDate}' ";
@@ -77,67 +84,131 @@ function statTimeBlock($outputPath, $beginDate, $endDate)
 	$outputFileHandle = fopen($outputPath, 'w');
 	// fputcsv($outputFileHandle, ['开始时间', '持续时间', '事件类别', '事件内容', '备注']);
 	$header = ['开始时间', '持续时间(h)', '事件类别', '事件内容', '备注'];
-	fputs($outputFileHandle, iconv('UTF-8', 'GBK//TRANSLIT', implode(",", $header) . "\n"));
+	outputToCsv($outputFileHandle, $header);
+	
 	while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 		$count++;
-		$row['duration_time'] = $row['duration_time'] / 60;
-		fputs($outputFileHandle, iconv('UTF-8', 'GBK//TRANSLIT', implode(",", $row). "\n"));
+		$row['duration_time'] = sprintf("%.2f", $row['duration_time'] / 60);
+		outputToCsv($outputFileHandle, $row);
 	}
 	echo "成功写入数据共" . $count . "条\n";
 	fclose($outputFileHandle);
+}
 
-	// 按事件类别统计
+function statByType($conn, $beginDate, $endDate)
+{
 	$statByTypeSql = "select sum(`duration_time`) / 60 as `total_time`,`event_type` from time_block where begin_time >= '{$beginDate}' and begin_time < '{$endDate}' group by  `event_type` order by `total_time` desc";
 	$statByTypeval = $conn->query($statByTypeSql);
-	echo "事件类别 ：耗时\n";
+	// echo $statByTypeSql . "\n";
+	$typeArr = [];
+	$sumTotalTime = 0;
 	while ($row = mysqli_fetch_array($statByTypeval, MYSQLI_ASSOC)) {
-		echo $row['event_type'] . " : " . $row['total_time'] . "\n";
+		$typeItem = [];
+		$typeItem['event_type'] = $row['event_type'];
+		$typeItem['total_time'] = sprintf("%.2f", $row['total_time']);
+		$sumTotalTime += $typeItem['total_time'];
+		$typeArr[] = $typeItem;
 	}
 
-	echo "\n\n";
-	// 按事件内容统计并输出
-	$statByContentSql = "select sum(`duration_time`) / 60 as `total_time`,`content` from time_block where begin_time >= '{$beginDate}' and begin_time < '{$endDate}'
-	and event_type in ('学习', '阅读') group by  `content` order by `total_time` desc";
+	foreach ($typeArr as &$type) {
+		$type['ratio'] = sprintf("%.4f", $type['total_time'] / $sumTotalTime) * 100 . "%";
+	}
+	unset($type);
+	echo "事件类别 ：耗时(共{$sumTotalTime}h) ：比例\n";
+	foreach ($typeArr as $type) {
+		echo $type['event_type'] . " : " . $type['total_time'] . " : " . $type['ratio'] . "\n";
+	}
+	echo "--------------------\n\n";
+
+	return $typeArr;
+}
+
+function statByContent($conn, $beginDate, $endDate, $types)
+{
+	if (!is_array($types)) {
+		$types = [$types];
+	}
+	$typesStr = implode("','", $types);
+	$statByContentSql = "select sum(`duration_time`) / 60 as `total_time`,`content` from time_block where begin_time >= '{$beginDate}' and begin_time < '{$endDate}' and event_type in ('{$typesStr}') group by  `content` order by `total_time` desc";
+	// echo $statByContentSql . "\n";
 	$statByContentval = $conn->query($statByContentSql);
-	echo "学习+阅读事件内容统计\n";
-	echo "事件内容 ：耗时\n";
+	if (!$statByContentval) {
+		echo "查询失败" . ",原因：" . mysqli_error($conn) . "\n";
+	}
+	
+	$contentArr = [];
+	$sumTotalTime = 0;
 	while ($row = mysqli_fetch_array($statByContentval, MYSQLI_ASSOC)) {
-		echo $row['content'] . " : " . $row['total_time'] . "\n";
+		$contentItem = [];
+		$contentItem['content'] = $row['content'];
+		$contentItem['total_time'] = sprintf("%.2f", $row['total_time']);
+		$sumTotalTime += $contentItem['total_time'];
+		$contentArr[] = $contentItem;
 	}
 
-	echo "\n\n";
+	foreach ($contentArr as &$content) {
+		$content['ratio'] = sprintf("%.4f", $content['total_time'] / $sumTotalTime) * 100 . "%";
+	}
+	unset($content);
+
+	echo implode("&", $types) . "事件内容统计\n";
+	echo "事件内容 ：耗时(共计{$sumTotalTime}h) ：比例\n";
+	foreach ($contentArr as $content) {
+		echo $content['content'] . " : " . $content['total_time'] . " : " . $content['ratio'] . "\n";
+	}
+	echo "--------------------\n\n";
+	return $contentArr;
+}
+
+function exportTimeBlock($outputPath, $beginDate, $endDate)
+{
+	$conn = connDb();
+	exportToExcel($conn, $outputPath, $beginDate, $endDate);
+	$conn->close();
+}
+
+function statTimeBlock($outputPath, $beginDate, $endDate)
+{
+	$conn = connDb();
+
+	$statFileHandle = fopen($outputPath, 'w');
+	// 按事件类别统计
+	$contentTypeArr = statByType($conn, $beginDate, $endDate);
+	$sumTotalTime = 0;
+	foreach ($contentTypeArr as $contentType) {
+		$sumTotalTime += $contentType['total_time'];
+	}
+	$header = ["事件类别", "耗时(共{$sumTotalTime}h)", "比例"];
+	outputToCsv($statFileHandle, $header);
+	foreach ($contentTypeArr as $contentType) {
+		outputToCsv($statFileHandle, $contentType);
+	}
+	outputToCsv($statFileHandle, ['', '', '']);
+
+
 	// 按事件内容统计并输出
-	$statByContentSql = "select sum(`duration_time`) / 60 as `total_time`,`content` from time_block where begin_time >= '{$beginDate}' and begin_time < '{$endDate}'
-	and event_type in ('工作') group by  `content` order by `total_time` desc";
-	$statByContentval = $conn->query($statByContentSql);
-	echo "工作事件内容统计\n";
-	echo "事件内容 ：耗时\n";
-	while ($row = mysqli_fetch_array($statByContentval, MYSQLI_ASSOC)) {
-		echo $row['content'] . " : " . $row['total_time'] . "\n";
-	}
+	$types = [['学习', '阅读'], '工作', '生活', '休闲娱乐'];
+	foreach ($types as $type) {
+		if (!is_array($type)) {
+			$type = [$type];
+		}
 
-	echo "\n\n";
-	// 按事件内容统计并输出
-	$statByContentSql = "select sum(`duration_time`) / 60 as `total_time`,`content` from time_block where begin_time >= '{$beginDate}' and begin_time < '{$endDate}'
-	and event_type in ('生活') group by  `content` order by `total_time` desc";
-	$statByContentval = $conn->query($statByContentSql);
-	echo "生活事件内容统计\n";
-	echo "事件内容 ：耗时\n";
-	while ($row = mysqli_fetch_array($statByContentval, MYSQLI_ASSOC)) {
-		echo $row['content'] . " : " . $row['total_time'] . "\n";
-	}
+		$contentArr = statByContent($conn, $beginDate, $endDate, $type);
+		$sumTotalTime = 0;
+		foreach ($contentArr as $content) {
+			$sumTotalTime += $content['total_time'];
+		}
 
-	echo "\n\n";
-	// 按事件内容统计并输出
-	$statByContentSql = "select sum(`duration_time`) / 60 as `total_time`,`content` from time_block where begin_time >= '{$beginDate}' and begin_time < '{$endDate}'
-	and event_type in ('休闲娱乐') group by  `content` order by `total_time` desc";
-	$statByContentval = $conn->query($statByContentSql);
-	echo "休闲娱乐事件内容统计\n";
-	echo "事件内容 ：耗时\n";
-	while ($row = mysqli_fetch_array($statByContentval, MYSQLI_ASSOC)) {
-		echo $row['content'] . " : " . $row['total_time'] . "\n";
+		$contentHeader = "事件内容(" . implode('&', $type) . ")";
+		$header = [$contentHeader, "耗时(共{$sumTotalTime}h)", "比例"];
+		outputToCsv($statFileHandle, $header);
+		foreach ($contentArr as $content) {
+			outputToCsv($statFileHandle, $content);
+		}
+		outputToCsv($statFileHandle, ['', '', '']);
 	}
-
+	fclose($statFileHandle);
+	
 	$conn->close();
 }
 
@@ -153,12 +224,14 @@ if ($argc >= 4) {
 	$endDate = $argv[3];
 	isset($argv[4]) && $path = $argv[4];
 }
-$outputPath = $basePath . "/export_timeblock_" . $beginDate . "~" . $endDate . ".csv";
+$outputPath = $basePath . "/timeblock_list_" . $beginDate . "~" . $endDate . ".csv";
+$statOutputPath = $basePath . "/timeblock_stat_" . $beginDate . "~" . $endDate . ".csv";
 //var_dump($action, $beginDate, $endDate, $path, $outputPath);
 if ($action == 'record') {
 	recordTimeBlock($path, $beginDate, $endDate);
 } elseif ($action == 'stat') {
-	statTimeBlock($outputPath, $beginDate, $endDate);
+	exportTimeBlock($outputPath, $beginDate, $endDate);
+	statTimeBlock($statOutputPath, $beginDate, $endDate);
 }
 
 
